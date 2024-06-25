76d0662282b35d5b449b9fe9e83d80c5b30a564e
Yosiak-alv <Josias_romero60@outlook.com>
2024-06-25 14:51:41 -0600

    Revert "Attempt 2 for 404 error fixing"
    This reverts commit 21c52af4d98feaf946dbaed197fbdaf6969c44e1.

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: './',
+  base: '/expectativa-siembra-documentation/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
