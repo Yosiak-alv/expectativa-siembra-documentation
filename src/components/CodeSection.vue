848a553b4cbadf08ed2262530eced6821745426a
Yosiak-alv <Josias_romero60@outlook.com>
2024-06-25 13:32:46 -0600

    Create CodeSection.vue

---
+++ src/components/CodeSection.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="overflow-hidden rounded" style="background: rgb(32, 46, 89);">
+        <div class="px-6 pt-6 pb-6 font-mono text-sm text-white">
+            <slot/>
+        </div>
+    </div>
+</template>
